99b404adbe087cb70f19c64f876177b1f9fed326
valgoncharov <goncharovvalentyn@gmail.com>
2022-11-07 22:54:09 +0300

    Homework - selenide first part. second try. Ok.

--- src/test/java/BestContributerInSelenide.java
+++ src/test/java/BestContributerInSelenide.java
@@ -1,18 +1,21 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static java.lang.Thread.sleep;
 
 public class BestContributerInSelenide {
     @Test
-    void andreiSolnrsevShouldBeTheFirstContributer(){
+    void andreiSolntsevShouldBeTheFirstContributer(){
         open("https://github.com/selenide/selenide");
         $(".Layout-sidebar").$(byText("Contributors"))
                 .ancestor(".BorderGrid-cell").$$("ul li")
                 .first().hover();
+        $$(".Popover-message").findBy(visible).shouldHave(text("Andrei Solntsev"));
 
     }
 }
